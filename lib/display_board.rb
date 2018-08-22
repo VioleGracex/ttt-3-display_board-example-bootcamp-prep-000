2b90f23e2528e00816dc61f37016b8bfc70dcb84
Viole Grace <highdevilxx@hotmail.com>
2018-08-22 13:55:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board(board)
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+def display_board
+  2.times do
+    puts "   |   |   "
+    puts "-----------"
+  end
+  puts "   |   |   "
 end
